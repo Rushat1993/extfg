9fa639a7f75ceca57d63175fb3b2713ec9794045
Rishat <risatnurullin53@gmail.com>
2022-07-19 17:58:41 +0300

    буmfa

---
+++ ta3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число N ");
+double x = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+while (count <= x)
+{
+    Console.WriteLine($"Куб числа {count}: {Math.Pow(count, 3)}");
+    count++;
+}
