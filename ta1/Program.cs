c040ad593cb77c7c29553d6f7a23dedd99ba0114
Rishat <risatnurullin53@gmail.com>
2022-07-19 17:57:21 +0300

    бум

---
+++ ta1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("");
+Console.Write("Введите число ");
+int uinput = Convert.ToInt32((Console.ReadLine()));
+int remainder = 0;
+int newnum = 0;
+for (int i = uinput; i > 0; i = (i / 10))
+{
+    remainder = i % 10;
+    newnum = (newnum*10) + remainder;
+}
+if (newnum == uinput)
+{
+    Console.WriteLine($"Число {uinput} является палиндромом");
+}
+else
+{
+    Console.WriteLine($"Число {uinput} не является палиндромом");
+}
